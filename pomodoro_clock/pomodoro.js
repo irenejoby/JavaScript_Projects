66c6e9dd9613a1156eebb50a3cd9bc80751372ce
Irene Cheryeth <30689512+irenejoby@users.noreply.github.com>
2018-01-25 19:27:14 +0100

    new commit-added breakTime

--- pomodoro_clock/pomodoro.js
+++ pomodoro_clock/pomodoro.js
@@ -10,10 +10,50 @@ $(document).ready(function(){
   $("#start").click(function(){
 
     var counter = setInterval(timer, 1000);
-    function timer(){
-      count -=1;
+    addTime *=60;
 
+    function timer(){
+      $("#start, #add5clock, #minus5clock, #minus5break, #add5break, #breakNum, #title1, #title2").hide();
+      $("#timeType").show();
+      $("#timeType").html("Session Time:");
+      addTime -=1;
+          if(addTime === 0){
+            buzzer.play();
+            clearInterval(counter);
+            var startBreak = setInterval(breakTimer, 1000);
+            $("#num").hide();
+          }
+      if(){
+
+      }else(){
+        
+      }
+
+      $("#num").html(addTime);
+
+        function breakTimer(){
+          $("#timeType").html("Break Time:");
+          $("#breakNum, #timeType").show();
+          breakTime -=1;
+          if(breakTime === 0){
+            clearInterval(startBreak);
+            buzzer.play();
+            $("#reset").show();
+            $("#breakNum, #timeType").hide();
+          }
+          $("#breakNum").html(breakTime);
+        }
     }
+
+  });
+
+  $("#reset").click(function(){
+    addTime = 25;
+    breakTime = 25;
+    $("#num").html(addTime);
+    $("#breakNum").html(breakTime);
+    $("#start, #add5clock, #minus5clock, #minus5break, #add5break, #breakNum, #num, #title1, #title2").show();
+    $("#reset, #timeType").hide();
   });
 
   $("#minus5clock").click(function(){
@@ -43,3 +83,6 @@ $(document).ready(function(){
     console.log(breakTime);
   });
 });
+// new method/convention
+// $("#buzzer").on("click",function(){
+// })﻿
